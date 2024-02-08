5f6bd2ccc1c6d9ea5dd536a4a3fba024db2b82b2
MRIDUL DAVESAR <88363654+MDavesar@users.noreply.github.com>
2024-02-08 11:19:50 +0530

    Create keywords.py

---
+++ preprocessing/keywords.py
@@ -0,0 +1,67 @@
+def keywords_list():
+    keyword_list=[]
+    keywords_ocr1=['parameter','result','widal','test','positive','antigen','paratyphi','agglutination','normal','diagnostic','range']
+    keyword_list.append(keywords_ocr1)
+    keywords_ocr2=['divyanshi','bloo','parameter','result','unit','w.b.c','polymorphs','lymphocytes','monocytes','hemoglobin','mcv','mch','pct','malaria','negative','range','eosinophils','bosophils','r.b.c','rdw','platelet','lacs','antigen','p.vivax','falciparum','clinically']
+    keyword_list.append(keywords_ocr2)
+    keywords_ocr3=['cbc','widal','serology','immunology','value','salmonella','typhi','widal','positive','unit','"bh"','"ah"','slide','titre','impression']
+    keyword_list.append(keywords_ocr3)
+    keywords_ocr4=['mri','brain','findings','myelination','gyral','monroe','lateral','impression','posterior','unremarkable','ventricles','brainstem','susceptibility','cerebellum','cortical','cisternal','bilateral','hypomyelination','dilation','hypomyelinating','lobes','tonsillar','herniation','supratentorial']
+    keyword_list.append(keywords_ocr4)
+    keywords_ocr5=['test','name','tsh','ultrasensitive','technology','value','comments','method','ustsh','electrohemiluminescene','immunoassay','end','disclaimer','e.c.l.i.a','sandwich','lipid','labcode','collected']
+    keyword_list.append(keywords_ocr5)
+    keywords_ocr6 = ['wbc','basophils','eosinophils','hematocrit','corpuscular','plateletcrit','volume','rdw-cv','lymphocytes','concentration','basophils','neutrophils']
+    keyword_list.append(keywords_ocr6)
+    keywords_ocr7 = ['alteration','tsh','pregnancy','trimester','american','clinically','unbound','report','values','test']
+    keyword_list.append(keywords_ocr7)
+    keywords_ocr8 = ['haematology','blood','differential','platelet','patient','r.b.c','corpuscular','count','biological','authorize']
+    keyword_list.append(keywords_ocr8)
+    keywords_ocr9 = ['hba1c','glycated','haemoglobin','prediabetic','clinically','hexose','reference','nonenzymatic']
+    keyword_list.append(keywords_ocr9)
+    keywords_ocr10 = ['jyoti','count','differential','complete','blood','mchc','platelets','wbc','packed','eosinophils','neutrophils','monocytes']
+    keyword_list.append(keywords_ocr10)
+    keywords_ocr11=['biochemistry' , 'serum' , 'serology' , 'typhi' , 'anyigen' , 'results' , 'units' , 'ah' , 'bh' , 'h' ,'o']
+    keyword_list.append(keywords_ocr11)
+    keywords_ocr12=['negative' , 'positive' , 'antigen' , 'gamma' , 'disease' , 'cdc' , 'mycobacterium' , 'tuberculosis' , 'mycobacterium' , 'radiography']
+    keyword_list.append(keywords_ocr12)
+    keywords_ocr13=['tsh' , 'serum' , 'circadian' , 'peak' , 'levels' , 'rare' , 'variant' , 'abnormal' , 'daigmosis' , 'thyroid' , 'pregnancy' , 'trimester' , 'nonthyroidal' , 'diseases' ]  
+    keyword_list.append(keywords_ocr13)
+    keywords_ocr14=['cbc' , 'blood' , 'haemoglobin' , 'erythrocyte' , 'leucocyte' , 'm.c.v.' , 'm.c.h.' , 'm.c.h.c' , 'p.c.v.'
+    , 'sugar' , 'basophils' , 'monocytes' , 'eosinophils' , 'lymphocytes' , 'neutrophils' , 'platelets']
+    keyword_list.append(keywords_ocr14)
+    keywords_ocr15=['urine' , 'acidic' , 'sediment' , 'albumin' , 'nitrite' , 'present' , 'blood' , 'microscopic' , 'bacteria' , 'cell' , 'erythrocytes' , 'spermatozoa' , 'epithelial' , 'calcium' , 'phosphate' , 'hyaline' , 'granular']
+    keyword_list.append(keywords_ocr15)
+    keywords_ocr16=['kidneys' , 'renal' , 'parenchyma' , 'calyces' , 'pelves' , 'ureters' , 'perirenal' , 'pararenal' , 'peritoneal' , 'fluid' , 'pelvic' , 'iliopsoas' , 'tissue' , 'abnormality' , 'ovaries' , 'mesentric' , 'lymphnodes']
+    keyword_list.append(keywords_ocr16)
+    keywords_ocr17=['typhi' , 'bh' , 'titres' , 'positive' , 'dose' , 'induration' , 'negative' , 'borderline' , 'mantoux' , 'mm' , 'ml' , 'tu']
+    keyword_list.append(keywords_ocr17)
+    keywords_ocr18=['tlc' , 'millions' , 'lac' , 'blood' , 'haemoglobin' , 'method' , 'mpv' , 'eosinophils' , 'neutrophils' , 'serology' , 'typhi' , 'negative' , 'widal' , 'platelet' , 'pcv']
+    keyword_list.append(keywords_ocr18)
+    keywords_ocr19=['haemoglobin' , 'picograms' , 'blood' , 'corpuscular' , 'basophils' , 'monocytes' , 'eosinophil' , 'r.b.c.' , 'w.b.c' , 'hb' , 'volume' , 'neutrophils' , 'cumm' , 'gm' , 'count']
+    keyword_list.append(keywords_ocr19)
+    keywords_ocr20=['thyroid' , 'tsh' , 'endocrinology' , 'circadian' , 'ultrasensitive' , 'hormone' , 'active' , 'rare' , 'variant' , 'test' , 'ng' , 'ug' , 'ml' , 'dl']
+    keyword_list.append(keywords_ocr20)
+    keywords_ocr21=['glucose' , 'plasma' , 'hexokinase' , 'test' , 'report' , 'mg' , 'dl' , 'units' , 'results']
+    keyword_list.append(keywords_ocr21)
+    keywords_ocr22=['thyroid' , 'serum' , 'chemiluminescent' , 'immunoassay' , 'tsh' , 'circadian' , 'hormone' , 'peak' , 'results' , 'test' , 'report' ]
+    keyword_list.append(keywords_ocr22)
+    keywords_ocr23=['glycosylated' , 'hemoglobin' , 'blood' , 'hplc' , 'ngsp' , 'eag' , 'glucode' , 'diabetes' , 'cell' , 'diabetic' , 'units' , 'test']
+    keyword_list.append(keywords_ocr23)
+    keywords_ocr24=['cbc' , 'blood' , 'mcv' , 'mch' , 'mchc' , 'rdw' , 'tlc' , 'cell' , 'count' , 'leukocyte' , 'basophils' , 'platelet' , 'monocytes' , 'lymphocytes' , 'leucocyte' , 'pg']
+    keyword_list.append(keywords_ocr24)
+    keywords_ocr25=['thyroid' , 'hormone' , 'pregnancy' , 'trimester' , 'tsh' , 'doapamine' , 'corticoids' , 'hypothyroidism' , 'ml' , 'serum' , 'blood' , 'units' , 'disease']
+    keyword_list.append(keywords_ocr25)
+    keywords_ocr26=['plasma' , 'glucose' , 'hexokinase' , 'sgpt' , 'liver' , 'injury' , 'damage' , 'prognosis' , 'disease' , 'hepatitis' , 'viruses' , 'creatinine' , 'serum']
+    keyword_list.append(keywords_ocr26)
+    keywords_ocr27=['hba1c' , 'glycated' , 'haemoglobin' , 'immunoturbidometry' , 'glucose' , 'risk' , 'retinopathy' , 'nephropathy' , 'cardiopathy' , 'neuropathy' , 'dcct' , 'diabetes' , 'eyes' , 'kidneys' , 'nerve' , 'eag' , 'mellitus']
+    keyword_list.append(keywords_ocr27)
+    keywords_ocr28=['immunoassay' , 'thyroid' , 'tsh' , 'hormone' , 'eclia' , 'pregnancy' , 'trimester' , 'circadian' , 'peak' , 'variant' , 'serum']
+    keyword_list.append(keywords_ocr28)
+    keyword_ocr29=['leucocyte' , 'd.l.c' , 'result' , 'erythrocyte' , 'sugar' , 'hct' , 'mcv' , 'mch' , 'haemoglobin' , 'basophils' , 'neutrophils' , 'eosinophils' , 'pdw' , 'pct' ]
+    keyword_list.append(keyword_ocr29)
+    keywords_ocr30=['blood' , 'haemogram' , 'hemoglobin' , 'leukocytes' , 'wbc' , 'morphology' , 'parasitetes' , 'neutropenia' , 'lymphocytosis' , 'erthrocytes' , 'decrease' , 'platelets' , 'monocytes' , 'basophils' ]
+    keyword_list.append(keywords_ocr30)
+    keywords_ocr31=['sputum' , 'semar' , 'acid' , 'bacilli' , 'results' , 'negative' , 'zncf' , 'staining' , 'method']
+    keyword_list.append(keywords_ocr31)
+
+    return keyword_list
